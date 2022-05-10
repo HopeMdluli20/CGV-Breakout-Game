0cd5afa428cd96199a61299e319f9e80d4a3d759
HopeMdluli20 <100839097+HopeMdluli20@users.noreply.github.com>
2022-05-10 21:06:03 +0200

    Newly Updated

--- pace.js
+++ pace.js
@@ -5,6 +5,11 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
+
+
+
+
+
   defaultOptions = {
     catchupTime: 100,
     initialRate: .03,
@@ -933,3 +938,118 @@
   }
 
 }).call(this);
+
+
+
+
+
+
+
+//Testing
+
+var imageRepository = new function() {
+	// Define images
+	this.background = new Image();
+
+	// Set images src
+	this.background.src = "Perfect.jpg";
+}
+
+function Drawable() {
+	this.init = function(x, y) {
+		// Default variables
+		this.x = x;
+		this.y = y;
+	}
+	this.speed = 0;
+	this.canvasWidth = 0;
+	this.canvasHeight = 0;
+
+	// Define abstract function to be implemented in child objects
+	this.draw = function() {
+	};
+}
+
+function Background() {
+	this.speed = 1; // Redefine speed of the background for panning
+	// Implement abstract function
+	this.draw = function() {
+		// Pan background
+		this.y += this.speed;
+		this.context.drawImage(imageRepository.background, this.x, this.y);
+		// Draw another image at the top edge of the first image
+		this.context.drawImage(imageRepository.background, this.x, this.y - this.canvasHeight);
+
+		// If the image scrolled off the screen, reset
+		if (this.y >= this.canvasHeight)
+			this.y = 0;
+	};
+}
+// Set Background to inherit properties from Drawable
+Background.prototype = new Drawable();
+
+function Game() {
+	/*
+	 * Gets canvas information and context and sets up all game
+	 * objects.
+	 * Returns true if the canvas is supported and false if it
+	 * is not. This is to stop the animation script from constantly
+	 * running on older browsers.
+	 */
+	this.init = function() {
+		// Get the canvas element
+		this.bgCanvas = document.getElementById('background');
+		// Test to see if canvas is supported
+		if (this.bgCanvas.getContext) {
+			this.bgContext = this.bgCanvas.getContext('2d');
+			// Initialize objects to contain their context and canvas
+			// information
+			Background.prototype.context = this.bgContext;
+			Background.prototype.canvasWidth = this.bgCanvas.width;
+			Background.prototype.canvasHeight = this.bgCanvas.height;
+			// Initialize the background object
+			this.background = new Background();
+			this.background.init(0,0); // Set draw point to 0,0
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	// Start the animation loop
+	this.start = function() {
+		animate();
+	};
+}
+
+
+function animate() {
+	requestAnimFrame( animate );
+	game.background.draw();
+}
+
+/**
+ * requestAnim shim layer by Paul Irish
+ * Finds the first API that works to optimize the animation loop,
+ * otherwise defaults to setTimeout().
+ */
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
+var game = new Game();
+
+function init() {
+	if(game.init())
+		game.start();
+}
+
+
+
